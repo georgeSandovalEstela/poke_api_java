abfd5f282eb44acf8ffe9737c6b7c551e0b7a10f
George Sandoval <george.sandoval.estela@gmail.com>
2022-07-06 20:52:52 -0500

    init commit & add list pokemons view

---
+++ src/View/Manager/ListController.java
@@ -0,0 +1,12 @@
+package View.Manager;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class ListController {
+    @FXML GridPane gridPane;
+
+    @FXML private void initialize(){
+
+    }
+}
